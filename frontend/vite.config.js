03b5588b06efff814c1baca954e6b27c075a97a5
Bob Lyons <bob.lyons.202@gmail.com>
2026-02-02 08:28:35 -0500

    fixes...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
     proxy: {
       '/uploads': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
+        target: 'http://backend:8000',
       },
     },
   },
